e4dc324efc9160179a9b777d1f131d9213b760ef
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-10 11:05:05 -0500

    add three routes to app

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,4 +1,15 @@
 class ExamplePagesController < ApplicationController
 
-  
+  def hey_mike
+render json: [{person: "Mike" , age: 35}, {person: "Jenny", age: 35}, {person: "Harvest", age: 12}]
+  end
+
+  def list_name
+    render json: {numbers: [1,2,3,4,5,6,7,8,9,10]}
+  end
+
+def weather
+  render json: {message: "here is the weather"}
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  
+get "/hey_mike", controller: "example_pages", action: "hey_mike"
+
+get "/weather", controller: "example_pages", action: "weather"
+
+get "/list_name", controller: "example_pages", action: "list_name"
+
 end
